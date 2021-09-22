b54c78e9d0a01641a0e978f58fdaf2e52b41dfd9
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-22 22:30:41 +0000

    wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,60 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordLookUp = (word, arr) => {
+    const horizontalJoin = arr.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+        }
     }
+};
+
+
+const reverseWordLookUp = (word, arr) => {
+    const horizontalJoin = arr.map(ls => ls.reverse().join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true
+        }
+    }
+};
+
+const transpose = (inputArr) => {
+   // transpose array
+   let array = [];
+   for (let i =  0; i < inputArr[0].length; i++) {
+     array.push([]);
+   }
+   for (let x = 0; x < inputArr.length; x++) {
+     for (let y = 0; y < inputArr[x].length; y++) {
+       array[y][x]  = inputArr[x][y];
+     }
+   }
+   return array;
 }
+
+const wordSearch = (letters, word) => { 
+    // Checks if word is written horizontally
+    if (wordLookUp(word, letters)) {
+        return true;
+    }
+
+    // Checks if word is written horizontally in reverse 
+    if (reverseWordLookUp(word, letters)) {
+        return true;
+    }
+
+    // Checks if word is written vertically
+    letters = transpose(letters);
+
+    if (wordLookUp(word, letters)) {
+        return true;
+    }
+    // Checks if word is written vertically in reverse 
+    if (reverseWordLookUp(word, letters)) {
+        return true;
+    }
+
+    return false;
+}
+
 
 module.exports = wordSearch
