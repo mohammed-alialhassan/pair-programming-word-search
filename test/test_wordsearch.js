b69ef0e01fca00d241e6a77959f73b250db81a37
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-22 22:29:51 +0000

    tests for word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,79 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  //Horizontal Tests
+  //Forward - Horizontal Test
+  it("should return true if the word is present (horizontally)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  //Backwards - Horizontal Test
+  it("result should return true if word is found backwards (horizontally)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'J', 'E', 'V', 'R', 'G'],
+      ['W', 'E', 'K', 'I', 'M', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'Y', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MIKE')
+
+    assert.isTrue(result);
+  });
+
+  //Vertical tests
+  //Forward - Vertical Test
+  it("should return true if the word is present (vertically)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'T', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'O', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'M', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'M', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'Y', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TOMMY')
+
+    assert.isTrue(result);
+  });
+
+  //Backwards - Vertical Test
+  it("result should return true if word is found backwards (vertically)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'J', 'E', 'V', 'H', 'G'],
+      ['W', 'E', 'K', 'I', 'M', 'E', 'T', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'I', 'B'],
+      ['U', 'B', 'T', 'Y', 'A', 'P', 'M', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'S', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SMITH')
+
+    assert.isTrue(result);
+  });
+
+
+  // Not found Horizontally OR Vertically 
+  it("should return false if the word is not present (vertical and horizontal check)", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,19 +92,4 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
-
-    assert.isTrue(result);
-  });
 });
